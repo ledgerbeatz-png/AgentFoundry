15d35f89b9bfb67cab31fe609964277e38e90c8d
ledgerbeatz-png <ledgerbeatz@gmail.com>
2026-09-20 20:12:58 +0200

    Export creator provider interfaces

--- src/agentfoundry/creator/__init__.py
+++ src/agentfoundry/creator/__init__.py
@@ -3,4 +3,11 @@
 from .profile import CreatorProfile
 from .memory import CreatorMemory
 
-__all__ = ["CreatorProfile", "CreatorMemory"]
+__all__ = [
+    "CreatorProfile", "CreatorMemory", "CreatorOrchestrator", "CreatorProviders",
+    "BrainProvider", "ImageProvider", "VoiceProvider", "VideoProvider",
+    "MediaRequest", "MediaResult",
+]
+
+from .orchestrator import CreatorOrchestrator, CreatorProviders
+from .providers import BrainProvider, ImageProvider, MediaRequest, MediaResult, VideoProvider, VoiceProvider
